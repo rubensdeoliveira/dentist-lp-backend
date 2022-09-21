43d2da604de7ceea32c7181895239c5b9b33b1c9
Rubens Junior <rubensojunior6@gmail.com>
2022-09-21 13:33:52 -0300

    feat: add public server

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => {
   return {
-    url: env("RENDER_EXTERNAL_URL", "")
+    url: env("PUBLIC_SERVER_URL", "")
   }
 };
--- dist/config/env/production/server.js
+++ dist/config/env/production/server.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => {
     return {
-        url: env("RENDER_EXTERNAL_URL", "")
+        url: env("PUBLIC_SERVER_URL", "")
     };
 };
