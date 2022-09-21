76dcd35e6aa027dd8886305f63572be9c392c712
Rubens Junior <rubensojunior6@gmail.com>
2022-09-21 09:42:34 -0300

    feat: add gmail email send

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,20 @@
-export default ({ env }) => ({
-  upload: {
-      config: {
-          provider: 'aws-s3',
-          providerOptions: {
-              accessKeyId: env('AWS_ACCESS_KEY_ID'),
-              secretAccessKey: env('AWS_ACCESS_SECRET'),
-              region: env('AWS_REGION'),
-              params: {
-                  Bucket: env('AWS_BUCKET_NAME'),
-              },
-          },
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        secure: true
       },
-  }
+      settings: {
+        defaultFrom: 'strapisendmailprovider@gmail.com',
+        defaultReplyTo: 'strapisendmailprovider@gmail.com',
+      },
+    },
+  },
 });
