ae6396dba02c603f4db6e9568214b1273d32c8b9
Rubens Junior <rubensojunior6@gmail.com>
2023-01-14 00:21:34 -0300

    feat: fix plugin

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,22 +1,4 @@
 module.exports = ({ env }) => ({
-    email: {
-        config: {
-            provider: 'nodemailer',
-            providerOptions: {
-                host: env('SMTP_HOST', 'smtp.example.com'),
-                port: env('SMTP_PORT', 587),
-                auth: {
-                    user: env('SMTP_USERNAME'),
-                    pass: env('SMTP_PASSWORD'),
-                },
-                secure: true
-            },
-            settings: {
-                defaultFrom: 'strapisendmailprovider@gmail.com',
-                defaultReplyTo: 'strapisendmailprovider@gmail.com',
-            },
-        },
-    },
     upload: {
         config: {
             provider: 'cloudinary',
